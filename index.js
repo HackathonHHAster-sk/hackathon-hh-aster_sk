6ab1bbe55b686d150efe29ba4da7ca4de324a081
Catharina <c.kenklies@satzmedia.de>
2015-05-23 16:18:42 +0200

    Panel fuer Button

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
-//var self = require('sdk/self');
+var { ToggleButton } = require('sdk/ui/button/toggle');
+var panels = require("sdk/panel");
+var self = require("sdk/self");
 
-// a dummy function, to show how tests work.
-// to see how to test this function, look at test/test-index.js
-//function dummy(text, callback) {
-//  callback(text);
-//}
-
-// exports.dummy = dummy;
-
-
-var buttons = require('sdk/ui/button/action');
-var tabs = require("sdk/tabs");
-
-var button = buttons.ActionButton({
-  id: "mozilla-link",
-  label: "Visit Mozilla",
+var button = ToggleButton({
+  id: "my-button",
+  label: "my button",
   icon: {
     "16": "./icon-16.png",
     "32": "./icon-32.png",
     "64": "./icon-64.png"
   },
-  onClick: handleClick
+  onChange: handleChange
 });
 
-function handleClick(state) {
-  tabs.open("https://www.mozilla.org/");
+var panel = panels.Panel({
+  contentURL: self.data.url("panel.html"),
+  onHide: handleHide
+});
+
+function handleChange(state) {
+  if (state.checked) {
+    panel.show({
+      position: button
+    });
+  }
+}
+
+function handleHide() {
+  button.state('window', {checked: false});
 }
